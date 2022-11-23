856e4ec66211c6cad47f112f47bc6d9609aa9abf
bkchr <bkchr@users.noreply.github.com>
2022-11-23 12:54:20 +0000

    deploy: ba01e9d32f47be9b76e7ad146bd42aea2b192964

--- implementors/pallet_aura/pallet/trait.Config.js
+++ implementors/pallet_aura/pallet/trait.Config.js
@@ -1,10 +1,10 @@
-(function() {var implementors = {
-"collectives_polkadot_runtime":[["impl Config for <a class=\"struct\" href=\"collectives_polkadot_runtime/struct.Runtime.html\" title=\"struct collectives_polkadot_runtime::Runtime\">Runtime</a>"]],
-"contracts_rococo_runtime":[["impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>"]],
-"parachain_template_runtime":[["impl Config for <a class=\"struct\" href=\"parachain_template_runtime/struct.Runtime.html\" title=\"struct parachain_template_runtime::Runtime\">Runtime</a>"]],
-"penpal_runtime":[["impl Config for <a class=\"struct\" href=\"penpal_runtime/struct.Runtime.html\" title=\"struct penpal_runtime::Runtime\">Runtime</a>"]],
-"rococo_parachain_runtime":[["impl Config for <a class=\"struct\" href=\"rococo_parachain_runtime/struct.Runtime.html\" title=\"struct rococo_parachain_runtime::Runtime\">Runtime</a>"]],
-"statemine_runtime":[["impl Config for <a class=\"struct\" href=\"statemine_runtime/struct.Runtime.html\" title=\"struct statemine_runtime::Runtime\">Runtime</a>"]],
-"statemint_runtime":[["impl Config for <a class=\"struct\" href=\"statemint_runtime/struct.Runtime.html\" title=\"struct statemint_runtime::Runtime\">Runtime</a>"]],
-"westmint_runtime":[["impl Config for <a class=\"struct\" href=\"westmint_runtime/struct.Runtime.html\" title=\"struct westmint_runtime::Runtime\">Runtime</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["collectives_polkadot_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"collectives_polkadot_runtime/struct.Runtime.html\" title=\"struct collectives_polkadot_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["collectives_polkadot_runtime::Runtime"]}];
+implementors["contracts_rococo_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["contracts_rococo_runtime::Runtime"]}];
+implementors["parachain_template_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"parachain_template_runtime/struct.Runtime.html\" title=\"struct parachain_template_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["parachain_template_runtime::Runtime"]}];
+implementors["penpal_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"penpal_runtime/struct.Runtime.html\" title=\"struct penpal_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["penpal_runtime::Runtime"]}];
+implementors["rococo_parachain_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"rococo_parachain_runtime/struct.Runtime.html\" title=\"struct rococo_parachain_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["rococo_parachain_runtime::Runtime"]}];
+implementors["statemine_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"statemine_runtime/struct.Runtime.html\" title=\"struct statemine_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["statemine_runtime::Runtime"]}];
+implementors["statemint_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"statemint_runtime/struct.Runtime.html\" title=\"struct statemint_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["statemint_runtime::Runtime"]}];
+implementors["westmint_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"westmint_runtime/struct.Runtime.html\" title=\"struct westmint_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["westmint_runtime::Runtime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/pallet_utility/pallet/trait.Config.js
+++ implementors/pallet_utility/pallet/trait.Config.js
@@ -1,7 +1,7 @@
-(function() {var implementors = {
-"collectives_polkadot_runtime":[["impl Config for <a class=\"struct\" href=\"collectives_polkadot_runtime/struct.Runtime.html\" title=\"struct collectives_polkadot_runtime::Runtime\">Runtime</a>"]],
-"contracts_rococo_runtime":[["impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>"]],
-"statemine_runtime":[["impl Config for <a class=\"struct\" href=\"statemine_runtime/struct.Runtime.html\" title=\"struct statemine_runtime::Runtime\">Runtime</a>"]],
-"statemint_runtime":[["impl Config for <a class=\"struct\" href=\"statemint_runtime/struct.Runtime.html\" title=\"struct statemint_runtime::Runtime\">Runtime</a>"]],
-"westmint_runtime":[["impl Config for <a class=\"struct\" href=\"westmint_runtime/struct.Runtime.html\" title=\"struct westmint_runtime::Runtime\">Runtime</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {};
+implementors["collectives_polkadot_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"collectives_polkadot_runtime/struct.Runtime.html\" title=\"struct collectives_polkadot_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["collectives_polkadot_runtime::Runtime"]}];
+implementors["contracts_rococo_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["contracts_rococo_runtime::Runtime"]}];
+implementors["statemine_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"statemine_runtime/struct.Runtime.html\" title=\"struct statemine_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["statemine_runtime::Runtime"]}];
+implementors["statemint_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"statemint_runtime/struct.Runtime.html\" title=\"struct statemint_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["statemint_runtime::Runtime"]}];
+implementors["westmint_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"westmint_runtime/struct.Runtime.html\" title=\"struct westmint_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["westmint_runtime::Runtime"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
