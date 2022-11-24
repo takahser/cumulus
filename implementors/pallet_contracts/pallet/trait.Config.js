bd00088336b975d29327d596b2fe33350b7866fd
bkchr <bkchr@users.noreply.github.com>
2022-11-24 10:56:36 +0000

    deploy: 32eb14c46aa4ddb29074c6ac88ae39aaf760c2be

--- implementors/pallet_contracts/pallet/trait.Config.js
+++ implementors/pallet_contracts/pallet/trait.Config.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["contracts_rococo_runtime"] = [{"text":"impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>","synthetic":false,"types":["contracts_rococo_runtime::Runtime"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"contracts_rococo_runtime":[["impl Config for <a class=\"struct\" href=\"contracts_rococo_runtime/struct.Runtime.html\" title=\"struct contracts_rococo_runtime::Runtime\">Runtime</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
